82e093aac4b8471ba78e6f2ede6857f1d5ca1938
unknown <vigneshchandru94@gmail.com>
2024-04-24 13:26:26 +0530

    Mobile update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,28 @@
 function Header() {
   return (
-    <div id="home" className="bg-[#212437] flex flex-row text-[#f9fdff] p-8 justify-between">
-      <div className="hidden md:block  w-[600px] text-center"> Vignesh Balasubramaniam </div>
+    <div
+      id="home"
+      className="bg-[#212437] flex flex-row text-[#f9fdff] p-8 justify-between"
+    >
+      <div className="hidden md:block w-[600px] text-center">
+        <span className="text-2xl font-extrabold text-[#f9fdff] italic">
+          Vignesh Balasubramainam
+        </span>
+      </div>
+
       <div className=" flex flex-row justify-center gap-10 w-full">
-        <a href="#home" className="hover:text-[#ff4b57]">Home</a>
-        <a href="#about" className="hover:text-[#ff4b57]">About</a>
-        <a href="#connect" className="hover:text-[#ff4b57]">Contact Us</a>
+        <a href="#home" className="hover:text-[#ff4b57]">
+          Home
+        </a>
+        <a href="#about" className="hover:text-[#ff4b57]">
+          About
+        </a>
+        <a href="#connect" className="hover:text-[#ff4b57]">
+          Contact Us
+        </a>
       </div>
     </div>
-  )
+  );
 }
 
 export default Header;
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -9,7 +9,7 @@ const handleDownload =()=>{
 const Home = () => {
     return <div className="text-[#f9fdff] px-10 md:px-[10rem] py-[4rem]">
         <p className="text-4xl md:text-6xl font-bold leading-[4rem]">
-            Hello, I'm <br /> a Software Test Engineer (SDET)
+            Hello, I'm <span className="md:hidden">Vignesh Balasubramainam</span> a Software Test Engineer (SDET)
         </p>
         <button onClick={handleDownload}className="bg-[#ff4b57] px-6 py-2 mt-10">
             Download CV
